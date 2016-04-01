5eb297f48ab62451e22381644ffe5885b079ca1a
Martin Arnaud <arnaud.martin1@insa-rennes.fr>
2016-04-01 14:28:22 +0200

    Parser articles

---
+++ Inquisitor/src/core/Parser.java
@@ -0,0 +1,60 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Parser {
+	private ArrayList<String> words = new ArrayList<String>();
+	private boolean[][] tab;
+
+	public ArrayList<String> getWords() {
+		return words;
+	}
+
+	public void setWords(ArrayList<String> words) {
+		this.words = words;
+	}
+
+	public boolean[][] getTab() {
+		return tab;
+	}
+
+	public void setTab(boolean[][] tab) {
+		this.tab = tab;
+	}
+
+	public boolean[][] run() {
+		Scanner sc = new Scanner(System.in);
+
+		while (sc.hasNextLine()) {
+			String s = sc.nextLine();
+			if (!words.contains(s)) {
+				words.add(s);
+			}
+		}
+
+		ArrayList<String> articles = new ArrayList<String>();
+		while (sc.hasNextLine()) {
+
+			articles.add(sc.nextLine());
+		}
+		tab = new boolean[articles.size()][words.size()];
+
+		int i = 0;
+		for (String article : articles) {
+			int j = 0;
+			for (String word : words) {
+				if (article.contains(word)) {
+					tab[i][j] = true;
+				} else {
+					tab[i][j] = false;
+				}
+				j++;
+			}
+			i++;
+		}
+
+		return tab;
+	}
+
+}
