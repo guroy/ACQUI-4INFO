3b29dd98fe7eb18ac237dee60c1cf0d7dda8812a
Adrien Reuzeau <adrien.reuzeau@insa-rennes.fr>
2016-04-01 15:29:55 +0200

    man + FP-growth

--- Inquisitor/src/core/FPGrowth.java
+++ Inquisitor/src/core/FPGrowth.java
@@ -1,14 +1,40 @@
 package core;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class FPGrowth implements ERAAlgorithm{
 
 	public void process(List<String> mots, boolean[][] map){
-		for(int i = 0; i<map.length ; i++){
-			for(int j = 0; j<map[0].length ; j++){
-				
+		
+		// boolean used to know if all the itemset are done
+		boolean finish = false;
+		
+		// tab for the frequence
+		int[] itemSetValue = new int[mots.size()];
+		
+		// minimum frequence = 0.25
+		int minsup = map.length / 4;
+		
+		// nombre de 1-itemset frequent
+		int count = 0;
+		
+		for(int i = 0; i < mots.size() ; i++){
+			for(int j = 0; j < map[i].length; j++){
+				if(map[i][j]){
+					(itemSetValue[i])++;
+				}
 			}
 		}
+		
+		// Step 2
+		HashMap<Integer,Integer> headerTable = new HashMap<Integer, Integer>();
+		
+		for(int i = 0 ; itemSetValue.length < 0 ; i++){
+			if(itemSetValue[i] > minsup){
+				headerTable.put(i, itemSetValue[i]);
+			}
+		}
+		
 	}
 }
