35dd6e2cdb023fc9c9deaa9c871ccbb0aa23c151
Adrien Reuzeau <adrien.reuzeau@insa-rennes.fr>
2016-04-01 14:24:56 +0200

    initializing main with arguments and use case message

--- Inquisitor/src/core/Main.java
+++ Inquisitor/src/core/Main.java
@@ -5,6 +5,28 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// -t = étude du ticket
+		// -a = étude de l'article
+		if (args[0].equals("-a")) {
+			if (args.length != 4) {
+				System.out.println("Utilisation : ");
+				System.out.println("inquisitor -a articles.txt mots.txt fichier.out");
+			} else {
+				// TODO
+			}
+		} else if (args[1].equals("-t")) {
+			if (args.length != 3) {
+				System.out.println("Utilisation : ");
+				System.out.println("inquisitor -t ticket.txt fichier.out");
+			} else {
+				// TODO
+			}
+		} else {
+			System.out.println("Utilisation : ");
+			System.out.println("inquisitor -a articles.txt mots.txt fichier.out");
+			System.out.println("ou");
+			System.out.println("inquisitor -t ticket.txt fichier.out");
+		}
 	}
 
 }
