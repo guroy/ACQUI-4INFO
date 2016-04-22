221f96b69337e21d11b5cde77fd91b5221ef0cdc
Martin Arnaud <arnaud.martin1@insa-rennes.fr>
2016-04-22 16:27:57 +0200

    Apriori

--- Inquisitor/src/core/Apriori.java
+++ Inquisitor/src/core/Apriori.java
@@ -9,6 +9,7 @@ public class Apriori implements ERAAlgorithm {
 	private Map<ArrayList<String>, Integer> result = new HashMap<ArrayList<String>, Integer>();
 
 	public void process(List<String> mots, boolean[][] map) {
+
 		ArrayList<ArrayList<String>> array = new ArrayList<ArrayList<String>>();
 
 		int index = 0;
@@ -16,28 +17,84 @@ public class Apriori implements ERAAlgorithm {
 			array.add(new ArrayList<String>());
 			array.get(index).add(mot);
 			result.put(array.get(index), 0);
+			index++;
 		}
 
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[0].length; j++) {
 				if (map[i][j]) {
-					int value = result.get(array.get(index));
+					int value = result.get(array.get(j));
 					result.put(array.get(index), value + 1);
 				}
 			}
 		}
 
 		L(mots, map, array, 1);
+		
 	}
 
-	
-	// current = item de l'étape précédantes, length taille des trucs de l'étape précédante
-	private Map<ArrayList<String>, Integer> L(List<String> mots,
-			boolean[][] map, ArrayList<ArrayList<String>> current, int length) {
-			
+	// current = item de l'étape précédantes, length taille des trucs de l'étape
+	// précédante
+	private void L(List<String> mots, boolean[][] map, ArrayList<ArrayList<String>> current,
+			int length) {
+
+		ArrayList<ArrayList<String>> next = new ArrayList<ArrayList<String>>();
+
+		for (int i = 0; i < current.size(); i++) {
+			ArrayList<String> a = current.get(i);
+
+			for (int j = i + 1; j < current.size(); j++) {
+				int test = 0;
+				ArrayList<String> b = current.get(j);
+				String toAdd = null;
+				for (String s : a) {
+					if (!b.contains(s)) {
+						test++;
+						if (test > 1) {
+							break;
+						}
+
+						toAdd = s;
+
+					}
+				}
+
+				if (test == 1) {
+					ArrayList<String> temp = new ArrayList<String>();
+					for (String s : a) {
+						temp.add(s);
+					}
+					temp.add(toAdd);
+					java.util.Collections.sort(temp);
+
+					if (!result.containsKey(temp)) {
+						int res = 0;
+						next.add(temp);
+						boolean valide = true;
+
+						for (int w = 0; w < map.length; w++) {
+							valide = true;
+							for (String s : temp) {
+								int index = mots.indexOf(s);
+								valide = map[w][index];
+								if (!valide) {
+									break;
+								}
+							}
+							if (valide) {
+								res++;
+							}
+						}
+
+						result.put(temp, res);
+					}
+				}
+
+			}
+		}
+
 		
-		
-		return null;
+		L(mots, map, next, length++);
 
 	}
 
